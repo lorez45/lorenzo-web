f64f6e2b99910ef2c5b797656a10bb8605df1325
lorez45 <lorenzosplinchi@gmail.com>
2026-03-16 15:19:02 +0100

    Update for.js

--- for.js
+++ for.js
@@ -1,5 +1,5 @@
 // stampa numeri da 100 a 0 con passo 20
 
-for (let i = 100; i >= 0; i -= 20) {
-    console.log(i);
+for (let i = 100; i >= 0; i -= 20) { // crea un ciclo: i parte da 100, continua finché i è >= 0, e diminuisce di 20 ad ogni iterazione
+    console.log(i); // stampa nella console il valore attuale della variabile i
 }
